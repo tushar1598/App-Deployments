da78f5581d4776bcc67ca87b8936730e79df4f74
Tushar Saini <tsaini425@gmail.com>
2024-10-06 11:31:53 +0530

    Codeial

--- frontend/src/components/auth.jsx
+++ frontend/src/components/auth.jsx
@@ -5,79 +5,47 @@ import { UserContext } from "../contexts/usercontext";
 export function AuthProfile({ children }) {
   const { data, authLoading } = useContext(UserContext);
   if (authLoading) return null;
-  return data ? (
-    children
-  ) : (
-    <Navigate to="https://app-deployments-frontend.onrender.com/users/sign-in" />
-  );
+  return data ? children : <Navigate to="/users/sign-in" />;
 }
 
 export function AuthSignUp({ children }) {
   const { data, authLoading } = useContext(UserContext);
   if (authLoading) return null;
-  return data ? (
-    <Navigate to="https://app-deployments-frontend.onrender.com/users/profile" />
-  ) : (
-    children
-  );
+  return data ? <Navigate to="/users/profile" /> : children;
 }
 
 export function AuthSignIn({ children }) {
   const { data, authLoading } = useContext(UserContext);
   if (authLoading) return null;
-  return data ? (
-    <Navigate to="https://app-deployments-frontend.onrender.com/users/profile" />
-  ) : (
-    children
-  );
+  return data ? <Navigate to="/users/profile" /> : children;
 }
 
 export function AuthResetPasswordLink({ children }) {
   const { data, authLoading } = useContext(UserContext);
   if (authLoading) return null;
-  return data ? (
-    <Navigate to="https://app-deployments-frontend.onrender.com/users/profile" />
-  ) : (
-    children
-  );
+  return data ? <Navigate to="/users/profile" /> : children;
 }
 
 export function AuthResetPassword({ children }) {
   const { data, authLoading } = useContext(UserContext);
   if (authLoading) return null;
-  return data ? (
-    <Navigate to="https://app-deployments-frontend.onrender.com/users/profile" />
-  ) : (
-    children
-  );
+  return data ? <Navigate to="/users/profile" /> : children;
 }
 
 export function AuthLogOut({ children }) {
   const { data, authLoading } = useContext(UserContext);
   if (authLoading) return null;
-  return data ? (
-    children
-  ) : (
-    <Navigate to="https://app-deployments-frontend.onrender.com/users/sign-in" />
-  );
+  return data ? children : <Navigate to="/users/sign-in" />;
 }
 
 export function AuthUpdate({ children }) {
   const { data, authLoading } = useContext(UserContext);
   if (authLoading) return null;
-  return data ? (
-    children
-  ) : (
-    <Navigate to="https://app-deployments-frontend.onrender.com/users/sign-in" />
-  );
+  return data ? children : <Navigate to="/users/sign-in" />;
 }
 
 export function AuthUpdatePassword({ children }) {
   const { data, authLoading } = useContext(UserContext);
   if (authLoading) return null;
-  return data ? (
-    children
-  ) : (
-    <Navigate to="https://app-deployments-frontend.onrender.com/users/sign-in" />
-  );
+  return data ? children : <Navigate to="/users/sign-in" />;
 }
