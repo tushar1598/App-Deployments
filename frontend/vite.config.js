9c476a8d80ab020d722acefb40526017cf19aebc
Tushar Saini <tsaini425@gmail.com>
2024-10-06 22:39:06 +0530

    Codeial

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,8 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react()],
-  build: {
-    outDir: "dist", // Ensure the output directory is correct
-  },
 });
